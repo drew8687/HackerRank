3632a7a1c0491a002e025686f1eb7afbba2cba68
drew8687 <eloifidriss>
2023-04-20 13:42:12 +0000

    commit 20/04

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -215,5 +215,51 @@ public class Main {
 
 
         }
+        //Getcurrency
+        /*
+        import java.util.*;
+import java.text.*;
+import java.util.Locale;
+
+public class Solution {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        Locale IND=new Locale("en", "IN");
+    NumberFormat u=NumberFormat.getCurrencyInstance(Locale.US);
+    NumberFormat i=NumberFormat.getCurrencyInstance(IND);
+    NumberFormat c=NumberFormat.getCurrencyInstance(Locale.CHINA);
+    NumberFormat f=NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+
+
+    String us=u.format(payment);
+    String india=i.format(payment);
+    String china=c.format(payment);
+    String france=f.format(payment);
+
+
+
+
+
+
+
+        // Write your code here.
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+
+    }
+}
+
+
+         */
+
     }
 }
